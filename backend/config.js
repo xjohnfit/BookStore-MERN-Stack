f82afafdba92cc993bdfa9743db499e379ea4b99
John Rocha <xjohnfitcodes@gmail.com>
2023-09-27 10:19:50 -0400

    BookStore-MERN-Stack init

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+'mongodb+srv://root:root@bookstore-mern-stack.zvdz2zm.mongodb.net/books-collection?retryWrites=true&w=majority';
---
+++ backend/index.js
@@ -0,0 +1,54 @@
+import express, { response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js' 
+
+const app = express();
+
+//Middleware for parsing req body
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    console.log(req);
+    return res.status(234).send('Welcome to MERN Stack')
+});
+
+//Route to save a new book
+app.post('/books', async (req, res) => {
+    try {
+        if(
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear
+        ) {
+            return res.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+        return res.status(201).send(book);
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message });
+    }
+});
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`APP is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
